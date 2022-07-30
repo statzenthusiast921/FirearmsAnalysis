0c1ed7850d40c6118e54bb34a81b4a299ce2ef94
Jon <31189528+statzenthusiast921@users.noreply.github.com>
2022-07-30 17:48:46 -0400

    Add files via upload

--- main/notebooks/app.py
+++ main/notebooks/app.py
@@ -1,6 +1,5 @@
 #Import packages
 
-from enum import unique
 import pandas as pd
 import numpy as np
 import os
@@ -8,7 +7,7 @@ import plotly.express as px
 import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 from dash import dash_table as dt
 
 
@@ -18,6 +17,19 @@ income_df = pd.read_csv('https://raw.githubusercontent.com/statzenthusiast921/Fi
 pop_df = pd.read_csv('https://raw.githubusercontent.com/statzenthusiast921/FirearmsAnalysis/main/main/data/states_and_pops.csv')
 prez_df = pd.read_csv('https://raw.githubusercontent.com/statzenthusiast921/FirearmsAnalysis/main/main/data/prez_voting.csv')
 own_df = pd.read_csv('https://raw.githubusercontent.com/statzenthusiast921/FirearmsAnalysis/main/main/data/own_df.csv')
+
+law_df = law_df.rename(
+    columns={
+        'Effective Date Year': 'Year',
+        'State Postal Abbreviation':'ST'
+    })
+law_df['Year'] = law_df['Year'].astype(str)
+law_type_choices = law_df['Law Class'].unique()
+
+#Fix Mississippi and DC labels
+law_df['State'] = np.where(law_df['State']=='MIssissippi', 'Mississippi', law_df['State'])
+law_df['State'] = np.where(law_df['State']=='DIstrict of Columbia', 'District of Columbia', law_df['State'])
+
 
 tabs_styles = {
     'height': '44px'
@@ -85,6 +97,30 @@ app.layout = html.Div([
         dcc.Tab(label='2nd Tab',value='tab-2',style=tab_style, selected_style=tab_selected_style,
             children=[
                 dbc.Row([
+                    dbc.Col([
+                        dcc.RadioItems(
+                                id='radio1',
+                                options=[
+                                    {'label': 'All Laws', 'value': 'All Laws'},
+                                    {'label': 'Permissive', 'value': 'Permissive'},
+                                    {'label': 'Restrictive', 'value': 'Restrictive'}
+                                ],
+                                value='All Laws',
+
+                                labelStyle={'display': 'block','text-align': 'left'}
+
+                        ),
+                        dcc.Graph(id='state_law_freq_map')
+                    ],width=6),
+                    dbc.Col([
+                        dcc.Dropdown(
+                            id='dropdown0',
+                            style={'color':'black'},
+                            options=[{'label': i, 'value': i} for i in law_type_choices],
+                            value=law_type_choices[0]
+                        ),
+                        dcc.Graph(id='law_types_timeline')
+                    ],width=6)
                 ])
             ]
         )
@@ -93,18 +129,103 @@ app.layout = html.Div([
 ])
 
 
-# #Configure Reactivity for Tab Colors
-# @app.callback(Output('tabs-content-inline', 'children'),
-#               Input('tabs-styled-with-inline', 'value'))
-# def render_content(tab):
-#     if tab == 'tab-1':
-#         return html.Div([
-#             html.H3('Tab content 1')
-#         ])
-#     elif tab == 'tab-2':
-#         return html.Div([
-#             html.H3('Tab content 2')
-#         ])
+
+@app.callback(
+    Output('state_law_freq_map','figure'),
+    Input('radio1','value'))
+
+def update_law_map(radio_select):
+
+   
+    if "All Laws" in radio_select:
+        map_df = pd.DataFrame(law_df[['State','ST']].value_counts()).reset_index()
+        map_df = map_df.rename(columns={0: 'Count'})
+
+        fig = px.choropleth(map_df,
+                    locations='ST',
+                    color='Count',
+                    template='plotly_dark',
+
+                    hover_name='State',
+                    locationmode='USA-states',
+                    labels={'Count':'# Laws Passed'},
+                    scope='usa')
+        return fig
+
+    elif "Permissive" in radio_select:
+        new_df = law_df[law_df['Effect']=="Permissive"]
+
+        map_df = pd.DataFrame(new_df[['State','ST']].value_counts()).reset_index()
+        map_df = map_df.rename(columns={0: 'Count'})
+
+        fig = px.choropleth(map_df,
+                    locations='ST',
+                    color='Count',
+                    template='plotly_dark',
+
+                    hover_name='State',
+                    locationmode='USA-states',
+                    labels={'Count':'# Laws Passed'},
+                    scope='usa')
+        return fig
+    else:
+
+        new_df = law_df[law_df['Effect']=="Restrictive"]
+
+        map_df = pd.DataFrame(new_df[['State','ST']].value_counts()).reset_index()
+        map_df = map_df.rename(columns={0: 'Count'})
+
+        fig = px.choropleth(map_df,
+                    locations='ST',
+                    color='Count',
+                    template='plotly_dark',
+
+                    hover_name='State',
+                    locationmode='USA-states',
+                    labels={'Count':'# Laws Passed'},
+                    scope='usa')
+        return fig
+
+@app.callback(
+    Output('law_types_timeline', 'figure'), 
+    Input('dropdown0', 'value') 
+)
+def timeline_graph(dd0):
+    filtered = law_df[law_df['Law Class']==dd0]
+    #filtered = law_df[law_df['Law Class']=="background checks"]
+
+    test = filtered[['Year','Law Class']]
+    test = filtered.groupby(['Year']).size().reset_index()
+    test = test.rename(columns={0: 'Count'})
+
+
+
+    fig = px.line(
+        test, 
+        x="Year", y="Count",
+        #markers=True,
+        #orientation='h',
+        template='plotly_dark',
+        #labels={'ones':'# Astronauts','country':'Country'},
+        #title=f'# Astronauts by Country in {slider0}',
+        #text_auto=True
+    )
+
+    
+    return fig
+
+#Configure Reactivity for Tab Colors
+@app.callback(Output('tabs-content-inline', 'children'),
+              Input('tabs-styled-with-inline', 'value'))
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
 
 #     elif tab == 'tab-3':
 #         return html.Div([
